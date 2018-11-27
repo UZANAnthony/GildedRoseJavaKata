f5559eaa6dbe99fc63a8ad963e98f1e6ef6c2f6a
UZANAnthony <anthony.uzan@gmail.com>
2018-11-27 14:20:06 +0100

    Class creation

---
+++ src/main/java/edu/insightr/gildedrose/AgedBrie.java
@@ -0,0 +1,7 @@
+package edu.insightr.gildedrose;
+
+public class AgedBrie extends Item{
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
---
+++ src/main/java/edu/insightr/gildedrose/Conjured.java
@@ -0,0 +1,7 @@
+package edu.insightr.gildedrose;
+
+public class Conjured extends Item{
+    public Conjured(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
